4f6076c23a7736948f17c0bd15a889099e241fc4
emmahf <hf.emma@gmail.com>
2014-05-18 15:02:04 +0200

    [Test script] enable the arm to follow the mouse pointer
    Test of how to later make the arm follow the mouse pointer. TODO: add this to the wiicontrol script in order to have the same effect when using the wiimote. Also includes small fixes for wiiinput smoothing.

---
+++ Assets/armMovementMouse.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class armMovementMouse : MonoBehaviour {
+
+	Quaternion targetrotation;
+	Plane targetPlane;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Create target plane
+		targetPlane = new Plane (-this.transform.forward, 
+		                               new Vector3(transform.position.x, transform.position.y, transform.position.z) + this.transform.forward*3);
+
+		float dist = 10.0f;
+
+		//Cast a ray from  mouse 
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		//Check if it hits the plane		
+		if (targetPlane.Raycast (ray, out dist)) {
+
+			//Get targetpoint as a point on the plane
+			var targetpoint = ray.GetPoint (dist);	
+			//Set appropriate rotation
+			targetrotation = Quaternion.LookRotation (targetpoint - transform.position, transform.up );
+			//Rotate arm
+			transform.rotation = targetrotation;
+
+		} 
+		else {
+			//Should not(!) be possible
+			Debug.Log("Not hitting the plane");
+		}
+	}
+
+}
