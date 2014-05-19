bd808564d63ff7d7c46d5b57afea18eb37e96487
emmahf <hf.emma@gmail.com>
2014-05-19 08:44:44 +0200

    [patch] wiimote grab and pointing fix
    still missing the purple ray thing

--- Assets/WiiInputMapping.cs
+++ Assets/WiiInputMapping.cs
@@ -96,6 +96,10 @@ public class WiiInputMapping : MonoBehaviour {
 	Queue<float> irYValues = new Queue<float>();
 	public const int IRVALUES = 20;
 
+	//Stuff for making the arm follow the wiimote
+	Quaternion targetrotation;
+	Plane targetPlane;
+
 
 	float getAverageIR(Queue<float> irvalues){
 		float average = 0;
@@ -115,9 +119,9 @@ public class WiiInputMapping : MonoBehaviour {
 
 		var smoothing = 1.0;
 		//Add values to queue if the difference is not too big (to avoid jitter and have more smooth motions
-		if(Math.Abs(irX - getAverageIR(irXValues)) < smoothing) 
+//		if(Math.Abs(irX - getAverageIR(irXValues)) < smoothing) 
 			irXValues.Enqueue(irX);
-		if(Math.Abs(irY - getAverageIR(irYValues)) < smoothing) 
+//		if(Math.Abs(irY - getAverageIR(irYValues)) < smoothing) 
 			irYValues.Enqueue(irY);
 		
 		//Get average new IR value ( again to ensure smooth motion since raw signal is very noisy)
@@ -125,6 +129,40 @@ public class WiiInputMapping : MonoBehaviour {
 		irY = getAverageIR(irYValues);
 		
 	}
+	
+	void smoothValueX(ref float irX){
+		
+		//Remove values from queue if it is full
+		if(irXValues.Count > IRVALUES){
+			irXValues.Dequeue();
+		}
+		
+		var smoothing = 1.0;
+		//Add values to queue if the difference is not too big (to avoid jitter and have more smooth motions
+		//		if(Math.Abs(irX - getAverageIR(irXValues)) < smoothing) 
+		irXValues.Enqueue(irX);
+		//Get average new IR value ( again to ensure smooth motion since raw signal is very noisy)
+		irX = getAverageIR(irXValues);
+		
+	}
+	
+	void smoothValueY(ref float irY){
+		
+		//Remove values from queue if it is full
+		if(irYValues.Count > IRVALUES){
+			irYValues.Dequeue();
+		}
+		
+		var smoothing = 1.0;
+		//Add values to queue if the difference is not too big (to avoid jitter and have more smooth motions
+		//		if(Math.Abs(irX - getAverageIR(irXValues)) < smoothing) 
+		irYValues.Enqueue(irY);
+		//Get average new IR value ( again to ensure smooth motion since raw signal is very noisy)
+		irY = getAverageIR(irYValues);
+		
+	}
+
+
 	
 	void Update() {
 		
@@ -137,6 +175,9 @@ public class WiiInputMapping : MonoBehaviour {
 			float roll = Mathf.Round(wiimote_getRoll(PLAYER1));
 			float p = Mathf.Round(wiimote_getPitch(PLAYER1));
 			float yaw = Mathf.Round(wiimote_getYaw(PLAYER1));
+
+
+			gyro.Set(roll, p, yaw);
 			float ir_x = wiimote_getIrX(PLAYER1);
 			float ir_y = wiimote_getIrY(PLAYER1);
 			display += "Wiimote " + PLAYER1 + "\n " +
@@ -206,15 +247,63 @@ public class WiiInputMapping : MonoBehaviour {
 			
 			//Debug.Log ("X: " + irX + " , Y: " + irY);
 
+
+			//Create target plane
+			targetPlane = new Plane (-this.transform.forward, 
+			                         new Vector3(transform.position.x, transform.position.y, transform.position.z) + this.transform.forward*3);
+
+			
+			float dist = 10.0f;
+			//Cast a ray from  mouse 
+			var screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+			Ray rayPlane = Camera.main.ScreenPointToRay(new Vector3(pointerX,pointerY,screenPoint.z));
+
+			//Check if it hits the plane		
+			if (targetPlane.Raycast (rayPlane, out dist)) {
+				
+				//Get targetpoint as a point on the plane
+				var targetpoint = rayPlane.GetPoint (dist);	
+				//Set appropriate rotation
+				targetrotation = Quaternion.LookRotation (targetpoint - transform.position, transform.up );
+				//Rotate arm
+				transform.rotation = targetrotation;
+				
+			} 
+			
+			irY = -p;
+			
+			if(irY > 100)
+				irY = 100;
+			if(irY < 30)
+				irY = 30;
+			
+			irY -= 30;
+			irY /= 70;
+
+
+			smoothValueY(ref irY);
+			pointerY = Mathf.RoundToInt(Screen.height*(irY));
+
+			
+//			Debug.Log( " , Y: " + pointerY );
+
+			//irY = Mathf.RoundToInt(Screen.height*(irY));
+			
 			//Check that IR information is available
-			if(!float.IsNaN(irX) && !float.IsNaN(irY) && irX != -100 && irY != -100 ){
+			//if(!float.IsNaN(irX) && !float.IsNaN(irY) && irX != -100 && irY != -100 ){
+				if(!float.IsNaN(irX) && irX != -100){
 
 				//Get smoothed values (average of last 5, without jumps etc)
-				smoothValues(ref irX, ref irY);
+//				smoothValues(ref irX, ref irY);
+				smoothValueX(ref irX);
 
 				//Rescale values to match screen size and get correct type
 				pointerX = Mathf.RoundToInt(Screen.width*(irX + 1)/2);
-				pointerY = Mathf.RoundToInt(Screen.height* ( 1 - irY)/2);
+
+//				//pointerY = Mathf.RoundToInt(Screen.height* ( 1 - irY)/2);
+//				pointerY = Mathf.RoundToInt(Screen.height*(irY));
+
+//				Debug.Log("X: " + pointerX + " , Y: " + pointerY );
 
 				//Check if wiimote A button is pressed (ie user wish to interact with the scene
 				if(wiimote_getButtonA(PLAYER1)){
@@ -246,7 +335,7 @@ public class WiiInputMapping : MonoBehaviour {
 							screenPosition = Camera.main.WorldToScreenPoint(interactingObject.transform.position);
 							
 							//calculate difference between object position and mouse position
-							offset = interactingObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(pointerX, -pointerY, screenPosition.z));
+							offset = interactingObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(pointerX, pointerY, screenPosition.z));
 
 						}
 						else{
@@ -266,7 +355,7 @@ public class WiiInputMapping : MonoBehaviour {
 						//interactingObject.transform.Translate();
 
 						
-						Vector3 curScreenPoint = new Vector3(pointerX, -pointerY, screenPosition.z);
+						Vector3 curScreenPoint = new Vector3(pointerX, pointerY, screenPosition.z);
 						curScreenPoint = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
 						
 						interactingObject.transform.position = curScreenPoint;
@@ -291,7 +380,7 @@ public class WiiInputMapping : MonoBehaviour {
 
 	void OnGUI(){
 		GUI.Label( new Rect(20,Screen.height-150, 500, 150), display);
-		GUI.Box (new Rect (pointerX, pointerY, 50, 50), cursorText);
+		GUI.Box (new Rect (pointerX, Screen.height -pointerY, 50, 50), cursorText);
 
 	}
 	
